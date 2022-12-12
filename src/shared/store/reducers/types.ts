a0211700b56f3e8314410c3b0b2e7d03f764f75c
azarro <azarro777@gmail.com>
2022-12-12 22:57:00 +0200

    ADD create types.ts and implement types for album and photo reducers

---
+++ src/shared/store/reducers/types.ts
@@ -0,0 +1,20 @@
+import { AsyncThunk } from '@reduxjs/toolkit';
+import IAlbum from 'shared/interfaces/album';
+import IPhoto from 'shared/interfaces/photo';
+
+type GenericAsyncThunk = AsyncThunk<unknown, unknown, any>;
+
+export type PendingAction = ReturnType<GenericAsyncThunk['pending']>;
+export type RejectedAction = ReturnType<GenericAsyncThunk['rejected']>;
+export type FulfilledAction = ReturnType<GenericAsyncThunk['fulfilled']>;
+
+type IError = {
+  code: number;
+  message: string;
+};
+
+export type IState = {
+  status: 'idle' | 'pending' | 'succeeded' | 'failed';
+  data: (IAlbum | IPhoto)[];
+  error: IError;
+};
