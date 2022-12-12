61174a893e72514ebc64df08639753106be696d7
azarro <azarro777@gmail.com>
2022-12-12 23:05:48 +0200

    MODIFY move duplicate logic to types.ts and index.ts

--- src/shared/store/reducers/PhotoReducer.ts
+++ src/shared/store/reducers/PhotoReducer.ts
@@ -1,35 +1,16 @@
-import { AnyAction, AsyncThunk, createSlice } from '@reduxjs/toolkit';
-import IPhoto from 'shared/interfaces/photo';
+import { createSlice } from '@reduxjs/toolkit';
+import { isFulfilledAction, isPendingAction, isRejectedAction } from '..';
 import {
   createPhoto,
   fetchPhotos,
   removePhoto,
   updatePhoto,
 } from './ActionCreators';
-
-type GenericAsyncThunk = AsyncThunk<unknown, unknown, any>;
-
-type PendingAction = ReturnType<GenericAsyncThunk['pending']>;
-type RejectedAction = ReturnType<GenericAsyncThunk['rejected']>;
-type FulfilledAction = ReturnType<GenericAsyncThunk['fulfilled']>;
-
-const isPendingAction = (action: AnyAction): action is PendingAction =>
-  action.type.endsWith('/pending');
-
-type IError = {
-  code: number;
-  message: string;
-};
-
-type IState = {
-  status: 'idle' | 'pending' | 'succeeded' | 'failed';
-  photos: IPhoto[];
-  error: IError;
-};
+import { IState } from './types';
 
 const initialState: IState = {
   status: 'idle',
-  photos: [],
+  data: [],
   error: { code: 0, message: '' },
 };
 
@@ -39,37 +20,29 @@ export const photoSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchPhotos.fulfilled, (state, action) => {
-      if (action.payload as IPhoto[]) {
-        state.photos = action.payload as IPhoto[];
-        state.status = 'succeeded';
-      }
+      state.data = action.payload;
+      state.status = 'succeeded';
     });
     builder.addCase(createPhoto.fulfilled, (state, action) => {
-      state.photos.push(action.payload as IPhoto);
+      state.data.push(action.payload);
     });
     builder.addCase(updatePhoto.fulfilled, (state, action) => {
-      state.photos.forEach((el) =>
+      state.data.forEach((el) =>
         el.id === action.payload.id ? el : action.payload
       );
     });
     builder.addCase(removePhoto.fulfilled, (state, action) => {
-      state.photos = state.photos.filter((el) => el.id !== action.payload.id);
+      state.data = state.data.filter((el) => el.id !== action.payload.id);
     });
     builder.addMatcher(isPendingAction, (state) => {
       state.status = 'pending';
     });
-    builder.addMatcher(
-      (action): action is RejectedAction => action.type.endsWith('rejected'),
-      (state) => {
-        state.status = 'failed';
-      }
-    );
-    builder.addMatcher<FulfilledAction>(
-      (action) => action.type.endsWith('/fulfilled'),
-      (state) => {
-        state.status = 'succeeded';
-      }
-    );
+    builder.addMatcher(isRejectedAction, (state) => {
+      state.status = 'failed';
+    });
+    builder.addMatcher(isFulfilledAction, (state) => {
+      state.status = 'succeeded';
+    });
   },
 });
 
