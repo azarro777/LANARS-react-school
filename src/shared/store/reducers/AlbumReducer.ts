09bf54018cfdc470117f26f2bfc17d352677913a
azarro <azarro777@gmail.com>
2023-01-02 13:01:06 +0200

    FIX fix addMatcher to determine which reducer

--- src/shared/store/reducers/AlbumReducer.ts
+++ src/shared/store/reducers/AlbumReducer.ts
@@ -1,10 +1,10 @@
 import {  createSlice } from '@reduxjs/toolkit';
 import { fulfilledAction, isFulfilledAction, isPendingAction, isRejectedAction, pendingAction, rejectedAction } from '..';
 import { createAlbum, fetchAlbums, removeAlbum, updateAlbum } from './ActionCreators';
-import { IState } from './types';
+import { DataStataus, IState } from './types';
 
 const initialState: IState = {
-  status: 'idle',
+  status: DataStataus.IDLE,
   data: [],
   errorMessage: '',
 };
@@ -20,7 +20,7 @@ export const albumSlice = createSlice({
       } else {
         state.data = [action.payload];
       }
-      state.status = 'succeeded';
+      state.status = DataStataus.SUCCEEDED;
     });
     builder.addCase(createAlbum.fulfilled, (state, action) => {
       state.data.push(action.payload);
@@ -33,9 +33,9 @@ export const albumSlice = createSlice({
     builder.addCase(removeAlbum.fulfilled, (state, action) => {
       state.data = state.data.filter((el: { id: number }) => el.id !== action.payload.id);
     });
-    builder.addMatcher(isPendingAction, pendingAction);
-    builder.addMatcher(isRejectedAction, rejectedAction);
-    builder.addMatcher(isFulfilledAction, fulfilledAction);
+    builder.addMatcher(isPendingAction('album/'), pendingAction);
+    builder.addMatcher(isRejectedAction('album/'), rejectedAction);
+    builder.addMatcher(isFulfilledAction('album/'), fulfilledAction);
   },
 });
 
