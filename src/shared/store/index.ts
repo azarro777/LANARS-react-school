dad761e380e8d5477e3e12cfce1646ad5c5938f8
azarro <azarro777@gmail.com>
2023-01-02 12:57:37 +0200

    ADD add check what type of reducer

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -2,6 +2,7 @@ import { AnyAction, combineReducers, configureStore } from '@reduxjs/toolkit';
 import AlbumReducer from './reducers/AlbumReducer';
 import PhotoReducer from './reducers/PhotoReducer';
 import {
+  DataStataus,
   FulfilledAction,
   IState,
   PendingAction,
@@ -20,31 +21,42 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 
-export function isPendingAction(action: AnyAction): action is PendingAction {
-  return action.type.endsWith('/pending');
+function typeOfReducer(action: AnyAction, type: string) {
+  return action.type.startsWith(type);
+}
+const isPending = (action: AnyAction) => action.type.endsWith('/pending');
+const isFulfilled = (action: AnyAction) => action.type.endsWith('/fulfilled');
+const isRejected = (action: AnyAction) => action.type.endsWith('/rejected');
+
+export function isPendingAction(type: string) {
+  return function (action: AnyAction): action is PendingAction {
+    return typeOfReducer(action, type) && isPending(action);
+  };
 }
 
-export function isRejectedAction(action: AnyAction): action is RejectedAction {
-  return action.type.endsWith('/rejected');
+export function isRejectedAction(type: string) {
+  return function (action: AnyAction): action is RejectedAction {
+    return typeOfReducer(action, type) && isRejected(action);
+  };
 }
 
-export function isFulfilledAction(
-  action: AnyAction
-): action is FulfilledAction {
-  return action.type.endsWith('/fulfilled');
+export function isFulfilledAction(type: string) {
+  return function (action: AnyAction): action is FulfilledAction {
+    return typeOfReducer(action, type) && isFulfilled(action);
+  };
 }
 
 export function pendingAction(state: IState): void {
-  state.status = 'pending';
+  state.status = DataStataus.PENDING;
   state.errorMessage = '';
 }
 
 export function rejectedAction(state: IState, action: AnyAction): void {
-  state.status = 'failed';
+  state.status = DataStataus.FAILED;
   state.errorMessage = `Error ${action.payload.code} ${action.payload.message}`;
 }
 
 export function fulfilledAction(state: IState): void {
-  state.status = 'succeeded';
+  state.status = DataStataus.SUCCEEDED;
   state.errorMessage = '';
 }
